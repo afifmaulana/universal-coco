6f337a5ce2a79be2c56bff37e7112e945100f534
Afif Maulana Iskandar <afifmaulanaiskandar@gmail.com>
2021-08-31 11:28:47 +0700

    +

--- resources/views/components/frontend/scripts.blade.php
+++ resources/views/components/frontend/scripts.blade.php
@@ -1,11 +1,11 @@
-<script src="assets/js/jquery-3.2.1.slim.min.js"></script>
-<script src="assets/js/jquery-3.6.0.js"></script>
-<script src="assets/js/popper.min.js"></script>
-<script src="assets/js/aos.js"></script>
-<script src="assets/js/lightbox.min.js"></script>
-<script src="assets/js/jquery.min.js"></script>
-<script src="assets/js/custom.js"></script>
-<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="{{ asset('assets/js/jquery-3.2.1.slim.min.js') }}"></script>
+<script src="{{ asset('assets/js/jquery-3.6.0.js') }}"></script>
+<script src="{{ asset('assets/js/popper.min.js') }}"></script>
+<script src="{{ asset('assets/js/aos.js') }}"></script>
+<script src="{{ asset('assets/js/lightbox.min.js') }}"></script>
+<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/js/custom.js') }}"></script>
+<script src="{{ asset('assets/bootstrap/js/bootstrap.min.js') }}"></script>
 {{-- <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script> --}}
 <script type="text/javascript">
     $(document).ready(function() {
